87567610b676080bbc464562439717e5b29499ca
george-garcia <40548850+george-garcia@users.noreply.github.com>
2023-04-14 01:37:49 -0500

    functionality is finished
    everything is done other than dark mode, it is responsive as well.

--- src/app/components/Card.jsx
+++ src/app/components/Card.jsx
@@ -2,19 +2,17 @@ import Image from "next/image";
 
 function Card({ image, title, description, tools }) {
   return (
-    <div>
-      <div className="flex flex-col items-center  shadow-lg p-10 rounded-xl my-10">
-        <Image src={image} width={100} height={100} />
-        <h3 className="text-lg font-medium pt-8 pb-2">Beautiful Designs</h3>
-        <p className="py-2">
-          Creating elegant designs suited for your needs following core design
-          theory.
-        </p>
-        <h4 className="py-4 text-teal-600">Design tools I use</h4>
-        <p className="text-gray-800 py-1">Photoshop</p>
-        <p className="text-gray-800 py-1">Illustrator</p>
-        <p className="text-gray-800 py-1">Figma</p>
-      </div>
+    <div className="flex flex-col items-center  shadow-lg p-10 rounded-xl my-10">
+      <Image src={image} width={100} height={100} />
+      <h3 className="text-lg font-medium pt-8 pb-2">Beautiful Designs</h3>
+      <p className="py-2">
+        Creating elegant designs suited for your needs following core design
+        theory.
+      </p>
+      <h4 className="py-4 text-teal-600">Design tools I use</h4>
+      <p className="text-gray-800 py-1">Photoshop</p>
+      <p className="text-gray-800 py-1">Illustrator</p>
+      <p className="text-gray-800 py-1">Figma</p>
     </div>
   );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,4 @@
+"use client";
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import { BsFillMoonStarsFill } from "react-icons/bs";
@@ -17,100 +18,144 @@ import web3 from "../../public/web3.png";
 import web4 from "../../public/web4.png";
 import web5 from "../../public/web5.png";
 import web6 from "../../public/web6.png";
+import { useState } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const [darkMode, setDarkMode] = useState(false);
+
   return (
-    <main className="bg-white px-10 md:px-20 lg:px-40">
-      <section className="min-h-screen">
-        <nav className="py-10 mb-12 flex justify-between">
-          <h1 className="text-xl font-burtons">Logo</h1>
-          <ul className="flex items-center gap-5">
-            <li>
-              <BsFillMoonStarsFill className="cursor-pointer text-xl" />
-            </li>
-            <li>
-              <a
-                className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded"
-                href="#"
-              >
-                Resume
-              </a>
-            </li>
-          </ul>
-        </nav>
-        <div className="text-center p-10 ">
-          <h2 className="text-5xl py-2 text-teal-600 font-medium">
-            George Garcia
-          </h2>
-          <h3 className="text-2xl py-2">Developer and designer.</h3>
-          <p className="text-md py-5 leading-8 text-gray-800">
-            Freelancer providing services for programming and something needs.
-            Join me down below and let's get cracking!
-          </p>
-        </div>
-        <div className="flex gap-16 justify-center text-5xl py-3 text-gray-600">
-          <AiFillTwitterCircle />
-          <AiFillGithub />
-          <AiFillLinkedin />
-        </div>
-        <div className="relative mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 mt-20 overflow-hidden">
-          <Image src={deved} alt="" layout="fill" objectFit="cover" />
-        </div>
-      </section>
+    <div className={darkMode ? "dark" : ""}>
+      <main className={"bg-white px-10 md:px-20 lg:px-40"}>
+        <section className="min-h-screen">
+          <nav className="py-10 mb-12 flex justify-between">
+            <h1 className="text-xl font-burtons">Logo</h1>
+            <ul className="flex items-center gap-5">
+              <li>
+                <BsFillMoonStarsFill className="cursor-pointer text-xl" />
+              </li>
+              <li>
+                <a
+                  className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded"
+                  href="#"
+                >
+                  Resume
+                </a>
+              </li>
+            </ul>
+          </nav>
+          <div className="text-center p-10 ">
+            <h2 className="text-5xl py-2 text-teal-600 font-medium md:text-6xl">
+              George Garcia
+            </h2>
+            <h3 className="text-2xl py-2 md:text-3xl">
+              Developer and designer.
+            </h3>
+            <p className="text-md py-5 leading-8 text-gray-800 md:text-xl max-w-xl mx-auto">
+              Freelancer providing services for programming and something needs.
+              Join me down below and let's get cracking!
+            </p>
+          </div>
+          <div className="flex gap-16 justify-center text-5xl py-3 text-gray-600">
+            <AiFillTwitterCircle />
+            <AiFillGithub />
+            <AiFillLinkedin />
+          </div>
+          <div className="relative mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 mt-20 overflow-hidden md:h-96 md:w-96">
+            <Image src={deved} alt="" layout="fill" objectFit="cover" />
+          </div>
+        </section>
 
-      <section>
-        <div>
-          <h3 className="text-3xl py-1">Services I offer</h3>
-          <p className="text-md py-2 leading-8 text-gray-800">
-            Since the beginning of my journey as a freelance developer, I've
-            done remote work for companies.
-          </p>
-          <p className="text-md py-2 leading-8 text-gray-800">
-            I offer from a wide range of services, including programming and
-            teaching.
-          </p>
-        </div>
+        <section>
+          <div>
+            <h3 className="text-3xl py-1">Services I offer</h3>
+            <p className="text-md py-2 leading-8 text-gray-800">
+              Since the beginning of my journey as a freelance developer, I've
+              done remote work for companies.
+            </p>
+            <p className="text-md py-2 leading-8 text-gray-800">
+              I offer from a wide range of services, including programming and
+              teaching.
+            </p>
+          </div>
+          <div className="lg:flex gap-10">
+            <Card image={design} />
+            <Card image={code} />
+            <Card image={consulting} />
+          </div>
+        </section>
 
-        <Card image={design} />
-        <Card image={code} />
-        <Card image={consulting} />
-      </section>
-
-      <section>
-        <div>
-          <h3 className="text-3xl py-1">Portfolio</h3>
-          <p className="text-md py-2 leading-8 text-gray-800">
-            Since the beginning of my journey as a freelance developer, I've
-            done remote work for companies.
-          </p>
-          <p className="text-md py-2 leading-8 text-gray-800">
-            I offer from a wide range of services, including programming and
-            teaching.
-          </p>
-        </div>
-        <div>
+        <section>
           <div>
-            <Image src={web1}></Image>
+            <h3 className="text-3xl py-1">Portfolio</h3>
+            <p className="text-md py-2 leading-8 text-gray-800">
+              Since the beginning of my journey as a freelance developer, I've
+              done remote work for companies.
+            </p>
+            <p className="text-md py-2 leading-8 text-gray-800">
+              I offer from a wide range of services, including programming and
+              teaching.
+            </p>
           </div>
-          <div>
-            <Image src={web2}></Image>
+          <div className="flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap">
+            <div className="basis-1/3 flex-1">
+              <Image
+                className="rounded-lg object-cover"
+                width={"100%"}
+                height={"100%"}
+                layout="responsive"
+                src={web1}
+              ></Image>
+            </div>
+            <div className="basis-1/3 flex-1">
+              <Image
+                className="rounded-lg object-cover"
+                width={"100%"}
+                height={"100%"}
+                layout="responsive"
+                src={web2}
+              ></Image>
+            </div>
+            <div className="basis-1/3 flex-1">
+              <Image
+                className="rounded-lg object-cover"
+                width={"100%"}
+                height={"100%"}
+                layout="responsive"
+                src={web3}
+              ></Image>
+            </div>
+            <div className="basis-1/3 flex-1">
+              <Image
+                className="rounded-lg object-cover"
+                width={"100%"}
+                height={"100%"}
+                layout="responsive"
+                src={web4}
+              ></Image>
+            </div>
+            <div className="basis-1/3 flex-1">
+              <Image
+                className="rounded-lg object-cover"
+                width={"100%"}
+                height={"100%"}
+                layout="responsive"
+                src={web5}
+              ></Image>
+            </div>
+            <div className="basis-1/3 flex-1">
+              <Image
+                className="rounded-lg object-cover"
+                width={"100%"}
+                height={"100%"}
+                layout="responsive"
+                src={web6}
+              ></Image>
+            </div>
           </div>
-          <div>
-            <Image src={web3}></Image>
-          </div>
-          <div>
-            <Image src={web4}></Image>
-          </div>
-          <div>
-            <Image src={web5}></Image>
-          </div>
-          <div>
-            <Image src={web6}></Image>
-          </div>
-        </div>
-      </section>
-    </main>
+        </section>
+      </main>
+    </div>
   );
 }
